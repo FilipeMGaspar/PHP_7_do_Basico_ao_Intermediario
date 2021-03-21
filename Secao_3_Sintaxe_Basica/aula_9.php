dfa8e8b6ef73fdd40b0e44c002c466af51b994f8
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-03-21 17:18:04 +0000

    Aula_9 Imprimindo Texto

--- Secao_3_Sintaxe_Basica/aula_9.php
+++ Secao_3_Sintaxe_Basica/aula_9.php
@@ -2,8 +2,9 @@
    $nome = "PHP 7" ;
    
    echo "Olá Mundo! <br>";
-   echo "Estou a aprender ".$nome."<br>";
+   echo "Estou a aprender <strong>".$nome."</strong><br>";
    echo"{$nome} é brutal! <br>";
+   print 'Olá Mundo! com print';
    
    
     
