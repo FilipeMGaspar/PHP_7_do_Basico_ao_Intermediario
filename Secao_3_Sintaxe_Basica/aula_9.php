40d49403c11500106b6f53c90fc4aba3012d98bc
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-03-21 17:20:22 +0000

    Aula_9 Imprimindo Texto

--- Secao_3_Sintaxe_Basica/aula_9.php
+++ Secao_3_Sintaxe_Basica/aula_9.php
@@ -1,10 +1,12 @@
 <?php
    $nome = "PHP 7" ;
    
-   echo "Olá Mundo! <br>";
+   echo "Olá Mundo! Com echo<br>";
    echo "Estou a aprender <strong>".$nome."</strong><br>";
    echo"{$nome} é brutal! <br>";
-   print 'Olá Mundo! com print';
+   print 'Olá Mundo! Com print <br>';
+   print "Print com {$nome} tem retorno <br>";
+   print "Concatenação com print ".$nome."<br>"
    
    
     
