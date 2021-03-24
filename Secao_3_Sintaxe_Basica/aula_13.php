03e18980fe2086819450076c8b7d1287e7dd3947
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-03-24 16:01:47 +0000

    Aula 13 - If, Else e Elseif

--- Secao_3_Sintaxe_Basica/aula_13.php
+++ Secao_3_Sintaxe_Basica/aula_13.php
@@ -1,9 +1,18 @@
 <?php
 
 $idade = 17;
+$senha = "1234";
 
-if($idade >= 18){
+/*if($idade >= 18){
     echo "OK";
 }else{
     echo "Inválido!!!";
+}*/
+
+if($senha == "123"){
+    echo "Acesso de nivel 1";
+}elseif ($senha == "1234") {
+    echo "Acesso de nivel 2";
+}else{
+    echo "Acesso Inválido";
 }
