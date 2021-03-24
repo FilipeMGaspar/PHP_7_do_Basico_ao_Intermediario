0280a4b62234e99c373477624298928e40e061b6
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-03-24 09:32:37 +0000

    Aula 10  Variáveis e Comentários

--- Secao_3_Sintaxe_Basica/aula_10.php
+++ Secao_3_Sintaxe_Basica/aula_10.php
@@ -1,7 +1,15 @@
 <?php
 
 $nome_completo = "Curso de PHP 7";
+$sexo = 'F';
+$salario = 745.85;
 
 echo $nome_completo."<br>";
 echo gettype($nome_completo)."<br>";
+
+echo $sexo."<br>";
+echo gettext($sexo)."<br>";
+
+
+
 
