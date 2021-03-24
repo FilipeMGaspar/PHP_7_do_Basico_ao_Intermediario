b4bae4021ea3cfd665e8feaea530d7023cf172f5
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-03-24 09:59:35 +0000

    Aula 11 - Operadores Matemáticos

--- Secao_3_Sintaxe_Basica/aula_11.php
+++ Secao_3_Sintaxe_Basica/aula_11.php
@@ -14,3 +14,16 @@ echo $divisao."<br>";
 echo "<br>". 2+2 ."<br>";
 ?>
 
+<!doctype html>
+<html lang="pt-PT">
+    <head>
+        <title>Operadores matemáticos</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <p>1 + 3 = <?=$adicao?></p> 
+        <p>5 - 2 = <?=$subtracao?></p>
+        <p>5 x 2 = <?=$multiplicacao?></p>
+        <p>10 / 2 = <?=$divisao?></p>
+    </body>
+</html>
