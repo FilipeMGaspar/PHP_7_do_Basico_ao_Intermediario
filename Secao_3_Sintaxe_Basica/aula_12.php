385a8b2870ddb64a6ebda9a8d54167bf9f00a0c0
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-03-24 11:00:03 +0000

    Aula 12 - Constante

---
+++ Secao_3_Sintaxe_Basica/aula_12.php
@@ -0,0 +1,7 @@
+<?php
+
+define('min', 17); //Constante chamada de 'min' com o valor '17'
+define('max', 45); //Constante chamada de 'max' com o valor '45'
+
+$idade = 25; //VAriavel com a idade do cliente '25'
+
