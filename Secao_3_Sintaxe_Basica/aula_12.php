0d8b5e14242eedbf96c396df550649d2005a3b4a
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-03-24 11:04:28 +0000

    Aula 12 - Constante

--- Secao_3_Sintaxe_Basica/aula_12.php
+++ Secao_3_Sintaxe_Basica/aula_12.php
@@ -5,3 +5,14 @@ define('max', 45); //Constante chamada de 'max' com o valor '45'
 
 $idade = 25; //VAriavel com a idade do cliente '25'
 
+//------------------------------------
+echo "MIN: ". min ."<br>";
+echo "MAX: ". max . "<br>";
+echo "IDADE: " . $idade . "<br><br>";
+
+//--------------------------------------
+if($idade >= min && $idade <= max){ // Validação de dados
+    echo "Acesso liberado.."; //Retorno verdadeiro
+}else{
+    echo "Acesso Bloqueado"; //Retorno falso
+}
